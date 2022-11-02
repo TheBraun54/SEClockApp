cb6fc4c1d6d6a01214e629892c1f6d578ee8cd6a
Zach <Lavakz98@uwosh.edu>
2022-11-02 00:01:20 -0500

    PlayPause button

--- Player.xaml.cs
+++ Player.xaml.cs
@@ -3,20 +3,34 @@ namespace SEClockApp;
 public partial class Player : ContentPage
 {
     //TimeSpan SelectedTime;
+    private TimeOnly time = new TimeOnly(00, 10, 00);
+
+
+    private bool isRunning;
+
     public Player()
     {
         InitializeComponent();
 
-
     }
 
-    public void Pause(object sender, EventArgs e)
+    public async void PlayPause(object sender, EventArgs e)
     {
+        isRunning = !isRunning;
 
+        PlayPauseButton.Text = isRunning ? "Pause" : "Play";
+
+        while (isRunning)
+        {
+            time = time.Add(TimeSpan.FromSeconds(-1));
+            Display.Text = $"{time.Minute:00}:{time.Second:00}";
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
     }
 
     public void Stop(object sender, EventArgs e)
     {
 
     }
+
 }
