1499fc0ada34e10f7fc1f629c6a22e9cbe751100
TheBraun54 <braunbrady@gmail.com>
2022-11-04 18:35:13 -0500

    Added a music image and the image buttons

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,20 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private async void SettingsClicked(object sender, EventArgs e)
 	{
-		count++;
+		await Shell.Current.GoToAsync($"//{nameof(Settings)}");
+	}
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+	private async void MusicClicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync($"//{nameof(Spotify)}");
 	}
 }
 
