d03373e00d83e9da22276f7de249ce3fc6b71a18
Zach <Lavakz98@uwosh.edu>
2022-11-15 21:58:17 -0600

    It plays an audio file using Naudio

--- Views/Player.xaml.cs
+++ Views/Player.xaml.cs
@@ -1,19 +1,37 @@
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
 namespace SEClockApp;
 
 public partial class Player : ContentPage
 {
-    //TimeSpan SelectedTime;
     private TimeOnly time = new TimeOnly(00, 10, 00);
 
     private bool isRunning;
 
+    private WaveOutEvent outputDevice;
+    private AudioFileReader audioFile;
+
+    public string AudioDir, AudioFile, AudioFilePath;
+
     public Player()
     {
         InitializeComponent();
+
+        // Clock
         isRunning = true;
         Clock();
+
+        // Audio
+        AudioDir = @"D:\";// from music select page
+        AudioFile = @"test.mp3";// from scanning files in AudioDir
+        AudioFilePath = Path.Combine(AudioDir, AudioFile);
+        Play();
     }
 
+    /// <summary>
+    /// Increments Display every second
+    /// </summary>
     public async void Clock()
     {
         while (isRunning)
@@ -24,7 +42,12 @@ public partial class Player : ContentPage
         }
     }
 
-    public void PlayPause(object sender, EventArgs e)
+    /// <summary>
+    /// Handler for the play and pause button
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public void PlayPauseHandler(object sender, EventArgs e)
     {
         isRunning = !isRunning;
         PlayPauseButton.Text = isRunning ? "II" : "\u25BA";
@@ -33,16 +56,59 @@ public partial class Player : ContentPage
             Clock();
             PlayPauseButton.BorderColor = Color.FromArgb("#F1E3F3");
             DisplayBorder.Stroke = Color.FromArgb("#F1E3F3");
+
+            Play();
         }
         else
         {
             PlayPauseButton.BorderColor = Color.FromArgb("#62BFED");
             DisplayBorder.Stroke = Color.FromArgb("#62BFED");
+
+            outputDevice?.Stop();
         }
     }
 
-    public async void Stop(object sender, EventArgs e)
+    /// <summary>
+    /// Sets outputDevice and audioFile if needed then plays audio
+    /// </summary>
+    public void Play()
     {
+        if (outputDevice == null)
+        {
+            outputDevice = new WaveOutEvent();
+            outputDevice.PlaybackStopped += OnPlaybackStopped;
+        }
+        if (audioFile == null)
+        {
+            audioFile = new AudioFileReader(AudioFilePath);
+            outputDevice.Init(audioFile);
+        }
+        outputDevice.Play();
+    }
+
+
+    /// <summary>
+    /// Removes audio data after outputDevice.PlaybackStopped
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+    {
+        outputDevice.Dispose();
+        outputDevice = null;
+        audioFile.Dispose();
+        audioFile = null;
+    }
+
+    /// <summary>
+    /// Handler for the Stop button
+    /// Stops audio and goes back a page
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public async void StopButtonHandler(object sender, EventArgs e)
+    {
+        outputDevice?.Stop();
         await Shell.Current.GoToAsync("..");
     }
 }
