1664dafc1a0b3f68bc1448b70a152afa75c93457
paulhwangj <pauljunhwang@gmail.com>
2022-11-30 14:43:54 -0600

    Added environmental variable for SPOTIFY_CLIENT_ID and confirmed that auth was made by printing currently logged-in user's name

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -15,6 +15,10 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
+        // TODO: move this somewhere more secure? where does this go?
+        // Sets the environment variable of our app's Client ID which is used in Settings.xaml.cs
+        Environment.SetEnvironmentVariable("SPOTIFY_CLIENT_ID", "be543f38f0f945f7a2384d7575434166");
+
         builder.UseMauiCommunityToolkit();
         return builder.Build();
     }
--- Views/Settings.xaml.cs
+++ Views/Settings.xaml.cs
@@ -2,36 +2,41 @@ using SEClockApp.PopUps;
 using CommunityToolkit.Maui.Views;
 using SpotifyAPI.Web;
 using static System.Formats.Asn1.AsnWriter;
+using SpotifyAPI.Web.Auth;
 
 namespace SEClockApp;
 
 public partial class Settings : ContentPage
 {
-    // Generates a secure random verifier of length 100 and its challenge
-    (string, string) verifierAndChallenge = PKCEUtil.GenerateCodes();
+    private static readonly string? clientId = Environment.GetEnvironmentVariable("SPOTIFY_CLIENT_ID");
+    // Make sure "http://localhost:5000/callback" is in your spotify application as redirect uri!
+    private static readonly EmbedIOAuthServer _server = new EmbedIOAuthServer(new Uri("http://localhost:5000/callback"), 5000);
 
     public Settings()
     {
         InitializeComponent();
     }
 
+    /// <summary>
+    /// Starts the server and processes the request to the whitelisted redirect uri
+    /// then when the application gets auth rights, OnImplicitGrantRecieved does the spotify calls
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private async void ConnectSpotifyHandler(object sender, EventArgs e)
     {
         try
         {
-            // Make sure "http://localhost:5000/callback" is in your applications redirect URIs!
-            var loginRequest = new LoginRequest(
-                new Uri("http://localhost:5000/callback"),
-                "be543f38f0f945f7a2384d7575434166", // TODO: shouldn't this be hidden somewhere?
-                LoginRequest.ResponseType.Code)
+            await _server.Start();
+
+            _server.ImplictGrantReceived += OnImplicitGrantReceived;
+            _server.ErrorReceived += OnErrorReceived;
+
+            var request = new LoginRequest(_server.BaseUri, clientId, LoginRequest.ResponseType.Token)
             {
-                CodeChallengeMethod = "S256",
-                CodeChallenge = verifierAndChallenge.Item2,
-                Scope = new[] { Scopes.PlaylistReadPrivate, Scopes.PlaylistReadCollaborative }
+                Scope = new List<string> { Scopes.UserReadEmail }
             };
-            // Redirects user to allow app to have access to their account
-            Uri uri = loginRequest.ToUri();
-            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+            BrowserUtil.Open(request.ToUri());
         }
         catch (Exception ex)
         {
@@ -39,19 +44,22 @@ public partial class Settings : ContentPage
         }
     }
 
-    // TODO: What? This method should be called from your web-server when the user visits "http://localhost:5000/callback"
-    // TODO: So do we have to run a local server for our application? (https://learn.microsoft.com/en-us/dotnet/maui/data-cloud/local-web-services?view=net-maui-6.0)
-    //public Task GetCallback(string code) // TOOD: OLD, doesn't return a task?
-    public async void GetCallback(string code) // TOOD: NEW
+    private static async Task OnImplicitGrantReceived(object sender, ImplictGrantResponse response)
     {
-        // Note that we use the verifier calculated above!
-        var initialResponse = await new OAuthClient().RequestToken(
-            // TODO: below is ClientID, hide it somewhere?
-          new PKCETokenRequest("be543f38f0f945f7a2384d7575434166", code, new Uri("http://localhost:5000"), verifierAndChallenge.Item1)
-        );
+        await _server.Stop();
+        var spotify = new SpotifyClient(response.AccessToken);
 
-        var spotify = new SpotifyClient(initialResponse.AccessToken);
-        // Also important for later: response.RefreshToken
+        // TODO: do calls with Spotify
+        var me = await spotify.UserProfile.Current(); // TODO: Delete?
+
+        // TODO: Delete below
+        System.Diagnostics.Debug.WriteLine($"Welcome {me.DisplayName}, you're authenticated!");
+    }
+
+    private static async Task OnErrorReceived(object sender, string error, string state)
+    {
+        Console.WriteLine($"Aborting authorization, error received: {error}");
+        await _server.Stop();
     }
 
 
