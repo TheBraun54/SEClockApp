f22cddfef2bd407b7e18392e886c213e6f06d0c5
paulhwangj <pauljunhwang@gmail.com>
2022-12-01 00:59:55 -0600

    Commented out non-functional portions for now

--- Views/Spotify.xaml.cs
+++ Views/Spotify.xaml.cs
@@ -12,14 +12,14 @@ public partial class Spotify : ContentPage
     public Spotify()
     {
         InitializeComponent();
-        PlaylistsLV.ItemsSource = MauiProgram.playlistVM.GetPlaylists();     // Sets the ItemSource of the ListView to the playlistsVM in MauiProgram.cs
+        //PlaylistsLV.ItemsSource = MauiProgram.playlistVM.GetPlaylists();     // Sets the ItemSource of the ListView to the playlistsVM in MauiProgram.cs
     }
 
-    public static void PopulatePlaylistGrid(IList<SimplePlaylist> retrievedPlaylists)
-    {
-        foreach (SimplePlaylist playlist in retrievedPlaylists)
-        {
-            MauiProgram.playlistVM.AddPlaylist(new Playlist(playlist.Name, 0, playlist.Images[0].Url, playlist.Id));
-        }
-    }
+    //public static void PopulatePlaylistGrid(IList<SimplePlaylist> retrievedPlaylists)
+    //{
+    //    foreach (SimplePlaylist playlist in retrievedPlaylists)
+    //    {
+    //        MauiProgram.playlistVM.AddPlaylist(new Playlist(playlist.Name, 0, playlist.Images[0].Url, playlist.Id));
+    //    }
+    //}
 }
